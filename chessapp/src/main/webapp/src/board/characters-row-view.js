d3e6de957c29a349bc902455244b12792b665de9
Radek Anuszewski <radekqwerty@gmail.com>
2017-04-10 21:24:27 +0200

    JSLint / JSHint has been configured and used on one file.

--- chessapp/src/main/webapp/src/board/characters-row-view.js
+++ chessapp/src/main/webapp/src/board/characters-row-view.js
@@ -1,24 +1,26 @@
 define(function (require) {
+    "use strict";
     const Mn = require("backbone.marionette");
     const Backbone = require("backbone");
     const CharacterView = require("./character-view");
-    const emptyElement = {character: ' '};
+    const emptyElement = {character: " "};
+    const _ = require("underscore");
 
     return Mn.CollectionView.extend({
         template: _.template(`<div></div><br>`),
         collection: new Backbone.Collection([
             emptyElement,
-            {character: 'a'},
-            {character: 'b'},
-            {character: 'c'},
-            {character: 'd'},
-            {character: 'e'},
-            {character: 'f'},
-            {character: 'g'},
-            {character: 'h'},
+            {character: "a"},
+            {character: "b"},
+            {character: "c"},
+            {character: "d"},
+            {character: "e"},
+            {character: "f"},
+            {character: "g"},
+            {character: "h"},
             emptyElement,
         ]),
         childView: CharacterView,
-        tagName: 'section',
+        tagName: "section",
     });
 });
