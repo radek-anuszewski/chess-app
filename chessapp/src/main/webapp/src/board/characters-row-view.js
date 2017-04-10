6fc7f3454eeb28d6e890ee18cbe36050a0c55aef
Radek Anuszewski <radekqwerty@gmail.com>
2017-04-10 21:35:55 +0200

    Spread operator has been used to introduce new constant.

--- chessapp/src/main/webapp/src/board/characters-row-view.js
+++ chessapp/src/main/webapp/src/board/characters-row-view.js
@@ -5,19 +5,22 @@ define(function (require) {
     const CharacterView = require("./character-view");
     const emptyElement = {character: " "};
     const _ = require("underscore");
+    const characters = [
+        {character: "a"},
+        {character: "b"},
+        {character: "c"},
+        {character: "d"},
+        {character: "e"},
+        {character: "f"},
+        {character: "g"},
+        {character: "h"},
+    ];
 
     return Mn.CollectionView.extend({
         template: _.template(`<div></div><br>`),
         collection: new Backbone.Collection([
             emptyElement,
-            {character: "a"},
-            {character: "b"},
-            {character: "c"},
-            {character: "d"},
-            {character: "e"},
-            {character: "f"},
-            {character: "g"},
-            {character: "h"},
+            ...characters,
             emptyElement,
         ]),
         childView: CharacterView,
