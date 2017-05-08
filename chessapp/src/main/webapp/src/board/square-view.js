46226d2564eb8f9b0d6ad57a713f262c4135953c
Radek Anuszewski <radekqwerty@gmail.com>
2017-05-08 22:00:25 +0200

    Chess displaying has been added.

--- chessapp/src/main/webapp/src/board/square-view.js
+++ chessapp/src/main/webapp/src/board/square-view.js
@@ -3,6 +3,7 @@ define(function (require) {
     const Mn = require("backbone.marionette");
     const _ = require("underscore");
     const Radio = require("backbone.radio");
+    const $ = require("jquery");
 
     return Mn.View.extend({
         template: _.template(`<span></span>`),
@@ -21,6 +22,56 @@ define(function (require) {
             if (this._isChessboardField()) {
                 this.$el.attr("role", "button");
                 this.$el.attr("tabindex", "0");
+                setTimeout(this._drawChessPiecesInUglyWay.bind(this), 0);
+            }
+        },
+        _drawChessPiecesInUglyWay: function () {
+            const rowNumber = this._getRowNumber();
+            const elements = this.$el.parent().find("p");
+            Array.from(elements).forEach(function bindIdInUglyWay(element, index) {
+                $(element).attr("id", rowNumber + "_" + index);
+            });
+            this._drawWhiteChessPiecesInUglyWay();
+            this._drawBlackChessPiecesInUglyWay();
+        },
+        _drawWhiteChessPiecesInUglyWay: function () {
+            const whiteFigures = [
+                "&#9814;",
+                "&#9816;",
+                "&#9815;",
+                "&#9813;",
+                "&#9812;",
+                "&#9815;",
+                "&#9816;",
+                "&#9814;",
+            ];
+            const rowNumber = this._getRowNumber();
+            if (Number(rowNumber) === 8) {
+                const fieldNumber = this._getFieldNumber() - 1;
+                this.$el.find("span").html(whiteFigures[fieldNumber]);
+            }
+            if (Number(rowNumber) === 7) {
+                this.$el.find("span").html("&#9817;");
+            }
+        },
+        _drawBlackChessPiecesInUglyWay: function () {
+            const blackFigures = [
+                "&#9820;",
+                "&#9822;",
+                "&#9821;",
+                "&#9819;",
+                "&#9818;",
+                "&#9821;",
+                "&#9822;",
+                "&#9820;",
+            ];
+            const rowNumber = this._getRowNumber();
+            if (Number(rowNumber) === 2) {
+                this.$el.find("span").html("&#9823;");
+            }
+            if (Number(rowNumber) === 1) {
+                const fieldNumber = this._getFieldNumber() - 1;
+                this.$el.find("span").html(blackFigures[fieldNumber]);
             }
         },
         _isChessboardField: function () {
@@ -40,6 +91,10 @@ define(function (require) {
         _getRowNumber: function () {
             const id = this.$el.parent().attr("id");
             return id.substr(id.length - 1);
+        },
+        _getFieldNumber: function () {
+            const id = this.$el.attr("id");
+            return Number(id.substr(id.length - 1));
         },
     });
 });
